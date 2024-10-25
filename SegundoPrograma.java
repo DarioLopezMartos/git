e3c6137d9195a901494a1ac06f713426625dd642
Dario López <dlopmar1411b@g.educaand.es>
2024-10-25 20:27:15 +0200

    Agrego el nuevo fichero en al rama nueva

---
+++ SegundoPrograma.java
@@ -0,0 +1,7 @@
+public class SegundoPrograma {
+    public static void main(String[] args) {
+        System.out.println("Este es el primer mensaje.");
+        System.out.println("Este es el segundo mensaje.");
+    }
+}
+
